169201756a69e39d2e837af3def73191c7c77360
streitlua <streitlua@gmail.com>
2017-07-21 14:34:17 +0200

    Ajouté status IN USE qui donne une heure à l'utilisateur pour passer à la prochainepage sinon elle sera considérée comme non comparée

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
 import web
 from web import form
+import time
 
 render = web.template.render('Templates/')
 
@@ -25,13 +26,15 @@ class compare:
 		if len(i.url)==2:
 			return render.compare(i.url[0], i.url[1])
 		else:
+			db.update('sites', where='STATUS="BUSY" AND DATE<' + str(time.time()-3600), STATUS=None, DATE=None)
 			url = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL LIMIT 1').list()
 			if not url:
 				return "No more sites to compare"
 			else:
 				temp = url[0]
+				
 				if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='DONE'):
-					db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='IN USE')
+					db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', DATE=time.time())
 				raise web.seeother('/compare?url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
 	def POST(self):
 		raise web.seeother('/compare')
--- sites.sql
+++ sites.sql
@@ -4,14 +4,15 @@ CREATE TABLE sites(
 ID SERIAL PRIMARY KEY NOT NULL,
 JAHIA TEXT,
 WORDPRESS TEXT,
-STATUS TEXT);
-INSERT INTO "sites" VALUES(0,'http://brisken-lab.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/briskenlab.html',NULL);
-INSERT INTO "sites" VALUES(1,'http://gr-co.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/gr-co.html',NULL);
-INSERT INTO "sites" VALUES(2,'http://lanes.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/lanes.html',NULL);
-INSERT INTO "sites" VALUES(3,'http://ccc.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/lcr.html',NULL);
-INSERT INTO "sites" VALUES(4,'http://mathgeom.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/mathgeom.html',NULL);
-INSERT INTO "sites" VALUES(5,'http://mmspg.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/mmspl.html',NULL);
-INSERT INTO "sites" VALUES(6,'http://rdsg.epfl.ch/','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/rdsg.html',NULL);
-INSERT INTO "sites" VALUES(7,'http://leure.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/reme.html',NULL);
-INSERT INTO "sites" VALUES(8,'http://scc.epfl.ch/page-22910-fr.html','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/sbsst.html',NULL);
+STATUS TEXT,
+DATE DATETIME);
+INSERT INTO "sites" VALUES(0,'http://brisken-lab.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/briskenlab.html', NULL, NULL);
+INSERT INTO "sites" VALUES(1,'http://gr-co.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/gr-co.html', NULL, NULL);
+INSERT INTO "sites" VALUES(2,'http://lanes.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/lanes.html', NULL, NULL);
+INSERT INTO "sites" VALUES(3,'http://ccc.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/lcr.html', NULL, NULL);
+INSERT INTO "sites" VALUES(4,'http://mathgeom.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/mathgeom.html', NULL, NULL);
+INSERT INTO "sites" VALUES(5,'http://mmspg.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/mmspl.html', NULL, NULL);
+INSERT INTO "sites" VALUES(6,'http://rdsg.epfl.ch/','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/rdsg.html', NULL, NULL);
+INSERT INTO "sites" VALUES(7,'http://leure.epfl.ch','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/reme.html', NULL, NULL);
+INSERT INTO "sites" VALUES(8,'http://scc.epfl.ch/page-22910-fr.html','http://10.92.104.248:8081/test-web-wordpress.epfl.ch/v1-testwp/sbsst.html', NULL, NULL);
 COMMIT;
