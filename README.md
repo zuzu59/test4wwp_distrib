07541d393e09130a0bd210cc3dec56808c64d137
streitlua <streitlua@gmail.com>
2017-07-25 11:53:55 +0200

    Adaptation à la lecture de la nouvelle liste de tous les sites à comparer

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Distributeur de tâches pour la comparaison visuelle de deux pages WEB pour le p
 
 ### Installation:
 **``./install.sh``**
-Nécessite aussi la présence d'un fichier credentials.csv où les liens à comparer se trouvent dans les deux premières colonnes de chaque ligne.
+Nécessite aussi la présence d'un fichier credentials.csv où les liens à comparer se trouvent dans les troisièmes et quatrièmes  colonnes de chaque ligne.
 Une fois ce fichier en place, faire **``./loadDB.sh``**
 
 ### Utilisation:
--- fillDB.py
+++ fillDB.py
@@ -8,8 +8,9 @@ try:
     i=0
     output.write("PRAGMA foreign_keys=OFF;\nBEGIN TRANSACTION;\nCREATE TABLE sites(ID SERIAL PRIMARY KEY NOT NULL,"
 		+"JAHIA TEXT,WORDPRESS TEXT,STATUS TEXT,DATE DATETIME);\n")
+    next(reader)
     for row in reader:
-    	output.write('INSERT INTO "sites" VALUES(' + str(i) + ', "'+ row[0] + '", "' + row[1]+ '", NULL, NULL);\n')
+    	output.write('INSERT INTO "sites" VALUES(' + str(i) + ', "'+ row[2] + '", "' + row[3]+ '", NULL, NULL);\n')
         i+=1
     output.write("COMMIT;")
     output.close()
