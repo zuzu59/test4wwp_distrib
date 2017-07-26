1b7ebe1e1af1d423684848d41e7be3f932ca1cc1
S-Rey <samuel.rey@epfl.ch>
2017-07-26 10:55:59 +0200

    ajout d'un select sur la page d'accueil

--- code.py
+++ code.py
@@ -12,6 +12,7 @@ urls = (
 	'/sendko', 'sendko'
 )
 
+
 db = web.database(dbn='sqlite', db='python.db')
 
 button = form.Form(
@@ -22,43 +23,45 @@ grace_period = 3600
 
 class index:
     def GET(self):
-    	return render.index()
-    	
+        names = db.query('SELECT NAME FROM users').list()
+        return render.index(names)
+
 class compare:
-	def GET(self):
-		i = web.input(url=[])
-		if len(i.url)==2:
-			return render.compare(i.url[0], i.url[1])
-		else:
-			#Verification de la date d'expiration de seances 'en cours'
-			db.update('sites', where='STATUS="BUSY" AND DATE<' + str(time.time()-grace_period), STATUS=None, DATE=None)
+    def GET(self):
+        s = web.input(select=[])
+        print(s)
+        i = web.input(url=[])
+        if len(i.url)==2:
+            return render.compare(i.url[0], i.url[1])
+        else:
+            #Verification de la date d'expiration de seances 'en cours'
+            db.update('sites', where='STATUS="BUSY" AND DATE<' + str(time.time()-grace_period), STATUS=None, DATE=None)
 			
-			urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL').list()
-			if not urls:
-				return "No more sites to compare"
-			else:
-				rdm_id = randint(0, len(urls))
-				temp = urls[rdm_id]
-				
-				if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='TESTED'):
-					db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', DATE=time.time())
-				raise web.seeother('/compare?url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
-	def POST(self):
-		raise web.seeother('/compare')
-		
+            urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL').list()
+            if not urls:
+                return "No more sites to compare"
+            else:
+                rdm_id = randint(0, len(urls))
+                temp = urls[rdm_id]
+	
+                if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='TESTED'):
+                    db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', DATE=time.time())
+                raise web.seeother('/compare?url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
+    def POST(self):
+        raise web.seeother('/compare')
 
 class next:
-	def POST(self):
-		i = web.input(url=None)
-		db.update('sites', where='JAHIA="' + i.url + '"', STATUS='TESTED', DATE=time.time())
-		raise web.seeother('/compare')
+    def POST(self):
+        i = web.input(url=None)
+        db.update('sites', where='JAHIA="' + i.url + '"', STATUS='TESTED', DATE=time.time())    
+        raise web.seeother('/compare')
 		
 class sendko:
-	def POST(self):
-		i = web.input(url=None)
-		db.update('sites', where='JAHIA="' + i.url + '"', STATUS='ERROR', DATE=time.time())
-		raise web.seeother('/compare')
+    def POST(self):
+        i = web.input(url=None)
+        db.update('sites', where='JAHIA="' + i.url + '"', STATUS='ERROR', DATE=time.time())
+        raise web.seeother('/compare')
 		
-if __name__ == "__main__": 
-    app = web.application(urls, globals())
+if __name__ == "__main__":  
+    app = web.application(urls, globals())  
     app.run()     
