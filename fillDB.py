692626ca8d5a64cdaafbd04af95c52f05446a542
S-Rey <samuel.rey@epfl.ch>
2017-07-26 14:39:47 +0200

    fillDB.py ajoute désormais une table users

--- fillDB.py
+++ fillDB.py
@@ -2,18 +2,28 @@ import os
 import errno
 import csv
 try:
-    origin = open('../credentials/credentials.csv')
+    # Insert urls
+    credentials = open('../credentials/credentials.csv')
     output = open('sites.sql', 'w')
-    reader = csv.reader(origin)
+    reader = csv.reader(credentials)
     i=0
     output.write("PRAGMA foreign_keys=OFF;\nBEGIN TRANSACTION;\nCREATE TABLE sites(ID SERIAL PRIMARY KEY NOT NULL,"
-		+"JAHIA TEXT,WORDPRESS TEXT,STATUS TEXT,DATE DATETIME);\n")
+		+"JAHIA TEXT,WORDPRESS TEXT,STATUS TEXT,USER TEXT, DATE DATETIME);\n")
     next(reader)
     for row in reader:
-    	output.write('INSERT INTO "sites" VALUES(' + str(i) + ', "'+ row[2] + '", "' + row[3]+ '", NULL, NULL);\n')
+    	output.write('INSERT INTO "sites" VALUES(' + str(i) + ', "'+ row[2] + '", "' + row[3]+ '", NULL, NULL, NULL);\n')
         i+=1
+    credentials.close()
+
+    # Insert users
+    users = open('users.csv')
+    reader = csv.reader(users)
+    output.write("CREATE TABLE users (ID SERIAL PRIMARY KEY NOT NULL, TEXT NAME);")
+    i = 0
+    for row in reader:
+        output.write('INSERT INTO "users" VALUES(' + str(i) + ',"' + row[1] +'");')  
+        i += 1
     output.write("COMMIT;")
     output.close()
-    origin.close()
 except IOError as ioex:
     print ('No credentials')
