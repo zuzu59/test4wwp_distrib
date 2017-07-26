a2f5750aa47433a9b3e340002baf19689cf4d5ea
S-Rey <samuel.rey@epfl.ch>
2017-07-26 16:12:13 +0200

    Utilisation d'id pour identification

--- code.py
+++ code.py
@@ -15,6 +15,7 @@ urls = (
 
 
 db = web.database(dbn='sqlite', db='python.db')
+names = db.query('SELECT ID, NAME FROM users').list()
 
 button = form.Form(
     form.Button("submit", type="submit", description="Next"),
@@ -22,33 +23,32 @@ button = form.Form(
 
 class index:
     def GET(self):
-        names = db.query('SELECT NAME FROM users').list()
         return render.index(names)
 
 class compare:
     def GET(self):
         urls = web.input(url=[]).url
-        name = web.input(name = '').name
+        user_id = web.input(user_id = 0).user_id
+        if (int(user_id) <= 0) or (int(user_id) > len(names)):
+            raise web.seeother('/')
         if len(urls)==2:
-            return render.compare(name, urls[0], urls[1])
+            return render.compare(user_id, urls[0], urls[1])
         else:
-
-            urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS="BUSY" AND USER="' + name + '";').list()
+            urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS="BUSY" AND USER_ID="' + user_id + '";').list()
      	    if not urls:
-		urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL;').list()
-		if not urls:
-                	return "No more sites to compare"
-
-            rdm_id = randint(0, len(urls)-1)
-            temp = urls[rdm_id]
-            if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='DONE'):
-            	db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', USER=name, DATE=time.time())
-            raise web.seeother('/compare?name=' + name + '&url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
+		        urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL;').list()
+        if not urls:
+            return "No more sites to compare"
+        rdm_id = randint(0, len(urls)-1)
+        temp = urls[rdm_id]
+        if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='DONE'):
+            db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', USER_ID=user_id, DATE=time.time())
+        raise web.seeother('/compare?user_id=' + user_id + '&url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
 
     def POST(self):
-        name = web.input(select=None).select
-        if name != "empty":
-            raise web.seeother('/compare?name=' + name)
+        user_id = web.input(select=None).select
+        if user_id != "empty":
+            raise web.seeother('/compare?user_id=' + user_id)
         else:
             raise web.seeother('/')
 
@@ -69,10 +69,10 @@ class connectionError:
 
 def update_status(status):
     url = web.input(url=None).url
-    name = web.input(name = '').name
-    if name:
+    user_id = web.input(user_id=0).user_id
+    if user_id != '0':
         db.update('sites', where='JAHIA="' + url + '"', STATUS=status, DATE=time.time())
-        raise web.seeother('/compare?name=' + name)
+        raise web.seeother('/compare?user_id=' + user_id)
     else:
         raise web.seeother('/')
 
--- fillDB.py
+++ fillDB.py
@@ -8,7 +8,7 @@ try:
     reader = csv.reader(credentials)
     i=0
     output.write("PRAGMA foreign_keys=OFF;\nBEGIN TRANSACTION;\nCREATE TABLE sites(ID SERIAL PRIMARY KEY NOT NULL,"
-		+"JAHIA TEXT,WORDPRESS TEXT,STATUS TEXT,USER TEXT, DATE DATETIME);\n")
+		+"JAHIA TEXT,WORDPRESS TEXT,STATUS TEXT,USER_ID INT, DATE DATETIME);\n")
     next(reader)
     for row in reader:
     	output.write('INSERT INTO "sites" VALUES(' + str(i) + ', "'+ row[2] + '", "' + row[3]+ '", NULL, NULL, NULL);\n')
@@ -20,9 +20,11 @@ try:
     reader = csv.reader(users)
     output.write("CREATE TABLE users (ID SERIAL PRIMARY KEY NOT NULL, NAME TEXT);")
     i = 0
+    next(reader)
     for row in reader:
-        output.write('INSERT INTO "users" VALUES(' + str(i) + ',"' + row[1] +'");')  
-        i += 1
+        if row[1] :
+            output.write('INSERT INTO "users" VALUES(' + row[0] + ',"' + row[1] + ' ' + row[2] +'");')  
+            i += 1
     output.write("COMMIT;")
     output.close()
 except IOError as ioex:
