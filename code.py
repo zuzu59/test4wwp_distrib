230d138c270c0a776f52522093b35699ed3bc295
streitlua <streitlua@gmail.com>
2017-07-26 14:15:38 +0200

    Changement de a priorité des sites à fetch

--- code.py
+++ code.py
@@ -35,16 +35,18 @@ class compare:
         else:
             #Verification de la date d'expiration de seances 'en cours'
             db.update('sites', where='STATUS="BUSY" AND DATE<' + str(time.time()-grace_period), STATUS=None, DATE=None)
-			
-            urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL').list()
-            if not urls:
-                return "No more sites to compare"
-            else:
-                rdm_id = randint(0, len(urls))
-                temp = urls[rdm_id]
-                if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='DONE'):
-                    db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', DATE=time.time())
-                raise web.seeother('/compare?name=' + name + '&url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
+
+            urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS="BUSY" AND USER="' + name + '";').list()
+     	    if not urls:
+		urls = db.query('SELECT JAHIA, WORDPRESS FROM sites WHERE STATUS IS NULL;').list()
+		if not urls:
+                	return "No more sites to compare"
+
+            rdm_id = randint(0, len(urls))
+            temp = urls[rdm_id]
+            if (db.query('SELECT STATUS FROM sites WHERE JAHIA="' + temp.JAHIA + '"')!='DONE'):
+            	db.update('sites', where='JAHIA="' + temp.JAHIA + '"', STATUS='BUSY', DATE=time.time())
+            raise web.seeother('/compare?name=' + name + '&url=' + temp.JAHIA + '&url=' + temp.WORDPRESS)
 
     def POST(self):
         name = web.input(select=None).select
@@ -62,22 +64,21 @@ class next:
 class emptyPage:
     def POST(self):
         update_status('EMPTY')
-        
+
 class connectionError:
     def POST(self):
         update_status('ERROR')
- 
+
 
 def update_status(status):
     url = web.input(url=None).url
     name = web.input(name = '').name
     if name:
-        db.update('sites', where='JAHIA="' + url + '"', STATUS=status, DATE=time.time())    
+        db.update('sites', where='JAHIA="' + url + '"', STATUS=status, DATE=time.time())
         raise web.seeother('/compare?name=' + name)
     else:
         raise web.seeother('/')
 
-		
-if __name__ == "__main__":  
-    app = web.application(urls, globals())  
-    app.run()     
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
